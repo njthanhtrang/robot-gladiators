d173e78775c7cd884f56d06ddc461513f45b4e16
njthanhtrang <njthanhtrang@users.noreply.github.com>
2021-02-11 11:19:52 -0800

    console log and fight fx

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,12 @@
-window.alert("This is an alert! Javascript is running!");
+// var playerName captures and stores response
+var playerName = window.prompt("What is your robot's name?");
+console.log(playerName);
+console.log("This logs a string, good for leaving a message");
+console.log(10+10);
+console.log("Our robot's name is " +playerName);
+
+function fight() {
+window.alert("The fight has begun!");
+}
+// must call fx
+// fight();
