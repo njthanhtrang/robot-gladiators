c97a2325743bbdcbff5d04187f165b6577fc9e80
njthanhtrang <njthanhtrang@users.noreply.github.com>
2021-02-16 15:18:02 -0800

    finished bug fix kind of

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,9 +1,3 @@
-// Game States
-// "WIN" - Player robot defeated all enemy-robots
-//    * Fight all enemy-robots
-//    * Defeat each enemy-robot
-// "LOSE" - Player robot's health is zero or less
-
 // function to start new game
 var startGame = function () {
     // reset player stats
@@ -23,9 +17,6 @@ var startGame = function () {
             pickedEnemyObj.health = randomNumber(40, 60);
 
             console.log(pickedEnemyObj);
-
-            // use debugger to pause script from running, check code
-            // debugger;
 
             // pass pickedEnemyObj object variable's value into fight function where it assumes value of enemy parameter
             fight(pickedEnemyObj);
@@ -92,7 +83,7 @@ var fight = function (enemy) {
         );
 
         // check enemy's health
-        if (enemy.Health <= 0) {
+        if (enemy.health <= 0) {
             window.alert(enemy.name + " has died!");
 
             // award player money for winning
@@ -109,7 +100,7 @@ var fight = function (enemy) {
             // leave while() loop since enemy is dead
             break;
         } else {
-            window.alert(enemy.name + " still has " + enemy.Health + " health left.");
+            window.alert(enemy.name + " still has " + enemy.health + " health left.");
         }
 
         var damage = randomNumber(enemy.attack - 3, enemy.attack);
@@ -151,12 +142,10 @@ var shop = function () {
         case "LEAVE":
         case "leave":
             window.alert("Leaving the store.");
-
             // do nothing, function ends
             break;
         default:
             window.alert("You did not pick a valid option. Try again.");
-
             // call shop() again to force player to pick a valid option
             shop();
             break;
@@ -164,43 +153,44 @@ var shop = function () {
 };
 
 // function generates random numeric value
-var randomNumber = function (min, max) {
+var randomNumber = function(min, max) {
     var value = Math.floor(Math.random() * (max - min) + min);
 
     return value;
 };
 // END GAME FUNCTIONS
 
+// function to set name
+var getPlayerName = function() {
+    var name = "";
+
+    while (name === "" || name === null) {
+        name = prompt("What is your robot's name?");
+    }
+    console.log("" Your robot's name is "" + name);
+    return name;
+};
+
 // GAME INFO / VARIABLES
 var playerInfo = {
-    name: window.prompt("What is your robot's name?"),
+    name: getPlayerName(),
     health: 100,
     attack: 10,
     money: 10,
-    reset: function () {
+    reset: function() {
         this.health = 100;
         this.money = 10;
         this.attack = 10;
     }, // comma!
-    refillHealth: function () {
-        if (this.money >= 7) {
-            window.alert("Refilling player's health by 20 for 7 dollars.");
-            this.health += 20;
-            this.money -= 7;
-        }
-        else {
-            window.alert("You don't have enough money!");
-        }
+    refillHealth: function() {
+        window.alert("Refilling player's health by 20 for 7 dollars.");
+        this.health += 20;
+        this.money -= 7;
     }, // comma!
-    upgradeAttack: function () {
-        if (this.money >= 7) {
-            window.alert("Upgrading player's attack by 6 for 7 dollars.");
-            this.attack += 6;
-            this.money -= 7;
-        }
-        else {
-            window.alert("You don't have enough money!");
-        }
+    upgradeAttack: function() {
+        window.alert("Upgrading player's attack by 6 for 7 dollars.");
+        this.attack += 6;
+        this.money -= 7;
     }
 };
 
